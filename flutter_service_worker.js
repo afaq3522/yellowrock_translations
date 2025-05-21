41e6b2d1787a1784749ae8257c1abb36292772d8
afaq-awan <aa@techscale.io>
2025-05-21 17:32:11 +0500

    added extract keys from text, disable enter while loading keys, responsivness for mobile

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "75a7d6bf8640d67a86671e34ade20154",
+const RESOURCES = {"flutter_bootstrap.js": "d68a659c76c2af6d393fb42f9f842e7e",
 "index.html": "cd5f3a4bd92c2a0aa4055bcaa470ebdf",
 "/": "cd5f3a4bd92c2a0aa4055bcaa470ebdf",
 "manifest.json": "d5695712b8c07904edb41174f1561ae6",
@@ -20,7 +20,7 @@ const RESOURCES = {"flutter_bootstrap.js": "75a7d6bf8640d67a86671e34ade20154",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "00a9840d91a1000cbca5414fcfcb7253",
+"main.dart.js": "a7335cb43f6ce13be49cd39e5c74f3bc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "168bfb7555bf2a42fe18d1292303d2ec",
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
