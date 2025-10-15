b85fa370e491f2b476bf92e3ade8bee778447863
afaq-awan <aa@techscale.io>
2025-10-15 17:49:34 +0500

    add dobby url

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d545c98d52729850a107261d000b778a",
+const RESOURCES = {"flutter_bootstrap.js": "ddf6be963d359d025bd946c2dbc58ace",
 "index.html": "cd5f3a4bd92c2a0aa4055bcaa470ebdf",
 "/": "cd5f3a4bd92c2a0aa4055bcaa470ebdf",
 "manifest.json": "d5695712b8c07904edb41174f1561ae6",
@@ -20,7 +20,7 @@ const RESOURCES = {"flutter_bootstrap.js": "d545c98d52729850a107261d000b778a",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "59231458c481c49c17442adcbdcdb7a4",
+"main.dart.js": "ee0dde1f555637a12690c2b5171de9c4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "168bfb7555bf2a42fe18d1292303d2ec",
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
